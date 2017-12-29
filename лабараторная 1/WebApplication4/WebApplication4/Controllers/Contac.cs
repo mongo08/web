f643c02f6d60f73aae5c303ec22dad5e284368fd
mongo08 <ruslanasharapov@yandex.ru>
2017-12-29 16:42:50 +0500

    web
    Проверте пожалуйста Доклад вместе делал с Ложкиной М.С

---
+++ лабараторная 1/WebApplication4/WebApplication4/Controllers/Contac.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class ContactController : Controller
+    {
+       
+        public IActionResult Send(MessageModel message)
+        {
+            try
+            {
+                var emailMessage = new MimeMessage();
+
+                emailMessage.From.Add(new MailboxAddress(message.Email, SMTP.Login));
+                emailMessage.To.Add(new MailboxAddress("", SMTP.Email));
+                emailMessage.Subject = message.Topic;
+                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                {
+                    Text = message.Text
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect(SMTP.Host, SMTP.Port, false);
+                    client.Authenticate(SMTP.Login, SMTP.Password);
+                    client.Send(emailMessage);
+
+                    client.Disconnect(true);
+                }
+
+                ViewBag.SuccessMessage = "Сообщение отправлено успешно!";
+            }
+            catch (Exception e)
+            {
+                ViewBag.ErrorMessage = e.Message;
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ лабараторная 1/WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication4.Models;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Send(MessageModel message)
+        {
+            try
+            {
+                var emailMessage = new MimeMessage();
+
+                emailMessage.From.Add(new MailboxAddress(message.Email, SMTP.Login));
+                emailMessage.To.Add(new MailboxAddress("", SMTP.Email));
+                emailMessage.Subject = message.Topic;
+                emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                {
+                    Text = message.Text
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.Connect(SMTP.Host, SMTP.Port, false);
+                    client.Authenticate(SMTP.Login, SMTP.Password);
+                    client.Send(emailMessage);
+
+                    client.Disconnect(true);
+                }
+
+                ViewBag.SuccessMessage = "Сообщение отправлено успешно!";
+            }
+            catch (Exception e)
+            {
+                ViewBag.ErrorMessage = e.Message;
+            }
+
+            return View("Index");
+        }
+
+    }
+}
